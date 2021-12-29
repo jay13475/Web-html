2510876cdc9003266df83dd2202223e567e99dbf
jay13475 <pondongeljie@gmail.com>
2021-12-29 23:42:27 +0800

    created page

---
+++ main.js
@@ -0,0 +1,30 @@
+function move() {
+		var x =document.getElementById("shapeId");
+		x.style.left="5px"
+		x.style.backgroundImage = "url('one.png')";
+		x.style.backgroundPosition = "center"; 
+		x.style.backgroundSize = "40px";
+		
+	}
+	 function move_one() {
+		var x =document.getElementById("shapeId");
+		x.style.left="99px";
+		x.style.backgroundImage = "url('two.png')";
+		x.style.backgroundPosition="center";
+		x.style.backgroundSize = "40px";  
+
+	}
+	 function move_two() {
+		var x =document.getElementById("shapeId")
+		x.style.left="210px";
+		x.style.backgroundImage = "url('three.png')"; 
+		x.style.backgroundPosition="center";
+		x.style.backgroundSize = "40px"; 
+	}
+	 function move_three() {
+		var x =document.getElementById("shapeId");
+		x.style.left="300px";
+		x.style.backgroundImage = "url('four.png')";
+		x.style.backgroundPosition = "center";
+		x.style.backgroundSize = "40px";
+	}
